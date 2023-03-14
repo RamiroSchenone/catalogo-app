5a3a8ad0afad46bee4564aa9d0a07001153c0ed8
Ramiro Agustin Schenone <ramacanalla21@gmail.com>
2023-03-14 20:47:50 -0300

    setup inicial, angular material, bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'catalogo', component: CatalogoComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'catalogo'},
+  {path: '**', pathMatch: 'full', redirectTo: 'catalogo'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './components/shared/shared.module';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CatalogoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
