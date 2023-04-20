9f15f7353a1f4ae5d5a009ddd64cff9b92950c90
RamiroSchenone <schenone.ramiro02@gmail.com>
2023-04-20 01:34:01 -0300

    Funcionalidades onFavorite & onDelete file-upload

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Image } from 'src/app/models/image.model';
 import { FileUploadService } from 'src/app/services/file-upload.service';
@@ -13,14 +13,14 @@ export class FileUploadComponent implements OnInit {
   @Input() extensionAccept: string;
   @Input() multiple: boolean = false;
 
-  hasImages: boolean = false;
-  // currentImages: Image[] = [];
+  currentImages: Image[] = [];
 
-  image: string | ArrayBuffer;
+  showAddImages: boolean = true;
+  // image: string | ArrayBuffer;
 
   constructor(
     private fileUploadService: FileUploadService,
-    private sanitizer: DomSanitizer
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {}
@@ -30,7 +30,7 @@ export class FileUploadComponent implements OnInit {
     if (files) {
       for (let i = 0; i < files.length; i++) {
         const currenItem: Image = files.item(i);
-        const currenImage: Image = files.item(i);
+        const currentImage: Image = files.item(i);
         if (currenItem) {
           // this.fileUploadService
           //   .postFile(files.item(i) as File)
@@ -38,16 +38,54 @@ export class FileUploadComponent implements OnInit {
           this.fileUploadService
             .getBase64ByImage(currenItem)
             .subscribe((res) => {
-              console.log(res);
-              this.image = res;
-              this.hasImages = true;
-              // this.currentImages.push(currenItem);
+              currentImage.base64 = res;
+              currentImage.isFavorite = false;
+
+              var existFavorite = this.currentImages.filter(x => x.isFavorite);
+              currentImage.isNotFavorite = existFavorite.length > 0 ? true : false;
+
+              this.currentImages.push(currentImage);
+
+              if (this.currentImages.length < 4) {
+                this.showAddImages = true;
+              } else {
+                this.showAddImages = false;
+              }
             });
         }
       }
     }
 
-    // (event.target as HTMLInputElement).files = null;
-    // (event.target as HTMLInputElement).value = '';
+    (event.target as HTMLInputElement).files = null;
+    (event.target as HTMLInputElement).value = '';
+  }
+
+  onDelete(index: number) {
+    this.currentImages.splice(index, 1);
+    if (this.currentImages.length <= 3) {
+      this.showAddImages = true;
+    }
+  }
+
+  onFavorite(index: number) {
+    const currentImage: Image = this.currentImages[index];
+    currentImage.isFavorite = !currentImage.isFavorite;
+
+    if (currentImage.isFavorite) {
+      var otherImages = this.currentImages.filter(
+        (image) => image.isFavorite == false
+      );
+      otherImages.forEach((image) => {
+        image.isNotFavorite = true;
+      });
+      this.currentImages = [];
+      this.currentImages.push(currentImage);
+      this.currentImages = [...this.currentImages, ...otherImages];
+    } else {
+      this.currentImages.forEach((image) => {
+        image.isFavorite = false;
+        image.isNotFavorite = false;
+      });
+    }
   }
 }
--- src/app/models/image.model.ts
+++ src/app/models/image.model.ts
@@ -1,3 +1,5 @@
 export class Image extends File {
     base64?: string | ArrayBuffer;
+    isFavorite?: boolean;
+    isNotFavorite?: boolean;
 }
